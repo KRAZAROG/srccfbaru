5c1319f37fb978434eefd39f48d64474a30052ec
KRAZAROG <dikaninja500@gmail.com>
2025-04-23 19:57:41 +0700

    new

---
+++ app/Views/main.php
@@ -0,0 +1,193 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SRC JF BARU</title>
+	<link rel="stylesheet" type="text/css" href="<?= base_url('css/bootstrap.min.css'); ?>">
+	<link rel="stylesheet" type="text/css" href="<?= base_url('css/style.css'); ?>">
+	<link rel="stylesheet" type="text/css" href="<?= base_url('css/bootstrap-theme.css'); ?>">
+	<script  src="<?= base_url('js/jquery.js'); ?>"></script>
+	<script  src="<?= base_url('js/bootstrap.min.js'); ?>"></script>
+
+
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row top">
+			<center>
+				<div class="col-md-4" style="padding: 3px;">
+					<span> <i class="glyphicon glyphicon-earphone"></i> 082279155310</span>
+				</div>
+
+
+				<div class="col-md-4"  style="padding: 3px;">
+					<span><i class="glyphicon glyphicon-envelope"></i> joefendikaSRC@gmail.com</span>
+				</div>
+
+
+				<div class="col-md-4"  style="padding: 3px;">
+					<span>SRC JF BARU Indonesia</span>
+				</div>
+			</center>
+		</div>
+	</div>
+
+	<nav class="navbar navbar-default" style="padding: 5px;">
+		<div class="container">
+
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#" style="color:rgb(180, 0, 0)"><b>SRC JF BARU</b></a>
+			</div>
+
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="index.php">Home</a></li>
+					<li><a href="produk.php">Produk</a></li>
+					<li><a href="about.php">Tentang Kami</a></li>
+					<li><a href="manual.php">Manual Aplikasi</a></li>
+											<li><a href="keranjang.php"><i class="glyphicon glyphicon-shopping-cart"></i> <b>[ 0 ]</b></a></li>
+
+												<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="glyphicon glyphicon-user"></i> hadi <span class="caret"></span></a>
+							<ul class="dropdown-menu">
+								<li><a href="proses/logout.php">Log Out</a></li>
+							</ul>
+						</li>
+
+										</ul>
+			</div><!-- /.navbar-collapse -->
+		</div><!-- /.container-fluid -->
+	</nav><!-- IMAGE -->
+<div class="container-fluid" style="margin: 0;padding: 0;">
+	<div class="image" style="margin-top: -21px">
+		<img src="image/home/1.jpg" style="width: 100%;  height: 650px;">
+	</div>
+</div>
+<br>
+<br>
+
+<!-- PRODUK TERBARU -->
+<div class="container">
+
+
+		<h4 class="text-center" style="font-family: arial; padding-top: 10px; padding-bottom: 10px; font-style: italic; line-height: 29px; border-top: 2px solid #ff8d87; border-bottom: 2px solid #ff8d87;">SRC JF BARU adalah warung yang menyediakan berbagai barang dengan harga terjangkau</h4>
+
+
+	<h2 style=" width: 100%; border-bottom: 4px solidrgb(180, 0, 0); margin-top: 80px;"><b>Produk Kami</b></h2>
+
+	<div class="row">
+					<div class="col-sm-6 col-md-4">
+				<div class="thumbnail">
+					<img src="image/produk/68011166ea820.jpg" >
+					<div class="caption">
+						<h3>Boneka</h3>
+						<h4>Rp.10,000</h4>
+						<div class="row">
+							<div class="col-md-6">
+								<a href="detail_produk.php?produk=P0001" class="btn btn-warning btn-block">Detail</a> 
+							</div>
+															<div class="col-md-6">
+									<a href="proses/add.php?produk=P0001&kd_cs=C0006&hal=1" class="btn btn-success btn-block" role="button"><i class="glyphicon glyphicon-shopping-cart"></i> Tambah</a>
+								</div>
+								
+						</div>
+
+					</div>
+				</div>
+			</div>
+						<div class="col-sm-6 col-md-4">
+				<div class="thumbnail">
+					<img src="image/produk/68011924b2c59.jpg" >
+					<div class="caption">
+						<h3>Silverqueen</h3>
+						<h4>Rp.15,000</h4>
+						<div class="row">
+							<div class="col-md-6">
+								<a href="detail_produk.php?produk=P0002" class="btn btn-warning btn-block">Detail</a> 
+							</div>
+															<div class="col-md-6">
+									<a href="proses/add.php?produk=P0002&kd_cs=C0006&hal=1" class="btn btn-success btn-block" role="button"><i class="glyphicon glyphicon-shopping-cart"></i> Tambah</a>
+								</div>
+								
+						</div>
+
+					</div>
+				</div>
+			</div>
+						<div class="col-sm-6 col-md-4">
+				<div class="thumbnail">
+					<img src="image/produk/680112370e98a.jpg" >
+					<div class="caption">
+						<h3>Tisu</h3>
+						<h4>Rp.3,000</h4>
+						<div class="row">
+							<div class="col-md-6">
+								<a href="detail_produk.php?produk=P0003" class="btn btn-warning btn-block">Detail</a> 
+							</div>
+															<div class="col-md-6">
+									<a href="proses/add.php?produk=P0003&kd_cs=C0006&hal=1" class="btn btn-success btn-block" role="button"><i class="glyphicon glyphicon-shopping-cart"></i> Tambah</a>
+								</div>
+								
+						</div>
+
+					</div>
+				</div>
+			</div>
+						<div class="col-sm-6 col-md-4">
+				<div class="thumbnail">
+					<img src="image/produk/68014266e6f2a.jpg" >
+					<div class="caption">
+						<h3>jam</h3>
+						<h4>Rp.50,000</h4>
+						<div class="row">
+							<div class="col-md-6">
+								<a href="detail_produk.php?produk=P0004" class="btn btn-warning btn-block">Detail</a> 
+							</div>
+															<div class="col-md-6">
+									<a href="proses/add.php?produk=P0004&kd_cs=C0006&hal=1" class="btn btn-success btn-block" role="button"><i class="glyphicon glyphicon-shopping-cart"></i> Tambah</a>
+								</div>
+								
+						</div>
+
+					</div>
+				</div>
+			</div>
+				</div>
+
+</div>
+<br>
+<br>
+<br>
+<br>
+
+	<footer style="border-top: 4px solidrgb(180, 0, 0);  ">
+		<div class="container" style="padding-bottom: 50px;">
+			<div class="row">
+				<div class="col-md-4">
+					<h3 style="color:rgb(180, 0, 0)"><b>SRC JF BARU</b></h3>
+					<p>Nambah Dadi, Kec. Terbanggi Besar, Kabupaten Lampung Tengah, Lampung</p>
+					<p><i class="glyphicon glyphicon-earphone"></i> 082279155310</p>
+					<p><i class="glyphicon glyphicon-envelope"></i> joefendikaSRC@gmail.com</p>
+				</div>
+				<div class="col-md-4">
+					<h5><b>Menu</b></h5>
+					<p><a href=""  style="color: #000">Produk</a></p>
+					<p><a href=""  style="color: #000">Tentang kami</a></p>
+					<p><a href=""  style="color: #000">Hubungi Kami</a></p>
+				</div>
+
+				<div class="col-md-4">
+					
+				</div>
+			</div>
+		</div>
+	</footer>
+
+</body>
+</html>
